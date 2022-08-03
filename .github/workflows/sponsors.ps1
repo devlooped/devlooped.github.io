b2ddd19099d27234abc6f4b59ca637c56f30b193
Daniel Cazzulino <daniel@cazzulino.com>
2022-08-03 02:33:49 -0300

    Create a directory, rather than a file

--- .github/workflows/sponsors.ps1
+++ .github/workflows/sponsors.ps1
@@ -38,7 +38,7 @@ $sponsors =
 $organizations = $sponsors | where { $_.sponsorEntity.teamsUrl -ne $null } | select -ExpandProperty sponsorEntity;
 $users = $sponsors | where { $_.sponsorEntity.teamsUrl -eq $null } | select -ExpandProperty sponsorEntity;
 
-New-Item -Path ".github/avatars" -ErrorAction Ignore
+mkdir ".github/avatars" -ErrorAction Ignore
 
 foreach ($node in $organizations) {
   $img = iwr ($node.avatarUrl + "&s=70");
